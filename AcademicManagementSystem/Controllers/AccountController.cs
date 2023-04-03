768920e00d1e8eed68134d2b7210cb404f242eeb
Metin ÖZTAY <metinoztay@gmail.com>
2023-04-03 22:26:29 +0300

    Login Page Theme Added

---
+++ AcademicManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcademicManagementSystem.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
