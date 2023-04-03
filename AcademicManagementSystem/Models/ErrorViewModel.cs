a074eca427e69ec9b812a82330c3bdc6cc4de4b5
Metin ÖZTAY <metinoztay@gmail.com>
2023-04-03 22:10:51 +0300

    First Commit

---
+++ AcademicManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcademicManagementSystem.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
